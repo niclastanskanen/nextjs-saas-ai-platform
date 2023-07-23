74301736e6f075832805b9311925d5bec99c1680
Niclas Tanskanen <niclas@niclass-air.home>
2023-07-23 10:47:10 +0200

    add pathname to sidebar to show active page on sidebar

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -1,24 +1,35 @@
-"use client"
+"use client";
 
-import { Menu } from "lucide-react"
+import { useEffect, useState } from "react";
+import { Menu } from "lucide-react";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import Sidebar from "@/components/Sidebar"
+import Sidebar from "@/components/Sidebar";
 
 const mobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <Sheet>
-        <SheetTrigger>
-            <Button variant="ghost" size="icon" className="md:hidden">
-                <Menu />
-            </Button>
-        </SheetTrigger>
-        <SheetContent side="left" className="p-0">
-            <Sidebar />
-        </SheetContent>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
     </Sheet>
-  )
-}
+  );
+};
 
-export default mobileSidebar
+export default mobileSidebar;
