402bc4149671e58b908fa4224fe02c0605d6246a
Niclas Tanskanen <niclas@niclass-air.home>
2023-07-24 12:14:08 +0200

    add settings page

---
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -0,0 +1,27 @@
+import { Settings } from "lucide-react"
+
+import Heading from "@/components/Heading"
+import { checkSubscription } from "@/lib/subscriptions"
+
+const SettingsPage = async () => {
+  const isPro = await checkSubscription();
+
+  return (
+    <div>
+      <Heading
+        title="Settings"
+        description="Manage your account settings."
+        icon={Settings}
+        iconColor="text-gray-700"
+        bgColor="bg-gray-700/10"
+      />
+      <div className="px-4 lg:px-8 space-y-4">
+        <div className="text-muted-foreground text-sm">
+          {isPro ? "You are subscribed to the Pro plan." : "You are not subscribed to the Pro plan."}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SettingsPage
