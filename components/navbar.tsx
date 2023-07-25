92752dc15836a19e7253cc249bf6fb281eb4971f
Niclas Tanskanen <niclas@niclass-air.home>
2023-07-25 10:11:57 +0200

    change tsconfig for vercel

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -11,11 +11,11 @@ const Navbar = async () => {
   return (
     <div className="flex items-center p-4">
       <MobileSidebar isPro={isPro} apiLimitCount={apiLimitCount} />
-        <div className="flex w-full justify-end">
-            <UserButton afterSignOutUrl="/" />
-        </div>
+      <div className="flex w-full justify-end">
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </div>
-  )
+   );
 }
 
 export default Navbar
